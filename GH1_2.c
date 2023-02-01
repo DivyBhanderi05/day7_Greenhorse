46b303e6053911e093c2a94dbd6d292eafc68b58
DivyBhanderi05 <123169804+DivyBhanderi05@users.noreply.github.com>
2023-02-01 17:14:31 +0530

    Update GH1_2.c

--- GH1_2.c
+++ GH1_2.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 main(){
-	printf("\t*\t*\t*\t*\t*\t*\t*\n");
+	printf("\t*\t*\t*\t*\t*\t*\t*\n\n");
 	printf("*\t*\t*\t*\t*\t*\t*\t*\n");
 	printf("*\t\t*\t\t\t\t\t*\n");
 	printf("*\t\t*\t\t*\t*\t*\t*\n");
